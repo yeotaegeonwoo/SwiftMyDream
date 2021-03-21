9847f1b84f0c9eb01ab08c5eb0c9d114df544e2f
Jaehan Lee <leejh17025@gmail.com>
2021-03-21 18:02:03 +0900

    공부

--- MyDream/ViewController.swift
+++ MyDream/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switchButton: UIButton!       //
+    @IBOutlet weak var flashImageView: UIImageView!
+    
+    var isOn = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    @IBAction func switchTapped(_ sender: Any) {  //switch를 눌렀을 때
+        isOn = !isOn
+        
+        if isOn == true {
+            switchButton.setImage(#imageLiteral(resourceName: "onSwitch"), for: .normal)
+            flashImageView.image = #imageLiteral(resourceName: "onBG")
+        }
+        else {
+            switchButton.setImage(#imageLiteral(resourceName: "offSwitch"), for: .normal)
+            flashImageView.image = #imageLiteral(resourceName: "offBG")
+        }
+        
+//        flashImageView.image = isOn ? #imageLiteral(resourceName: "onBG") : #imageLiteral(resourceName: "offBG")
+//        switchButton.setImage(isOn ? #imageLiteral(resourceName: "onSwitch") : #imageLiteral(resourceName: "offSwitch"), for: .normal)
+
+    }
 
 }
 
